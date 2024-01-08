1600af7c6568a4548f11b8a8a06156a08bf00003
Craig Elliott <craigelliottdev@gmail.com>
2024-01-08 09:05:22 +0100

    Refactor

---
+++ src/UI/Header.svelte
@@ -0,0 +1,20 @@
+<style>
+    h1, h2 {
+		color: #FE0000;
+		font-style: italic;
+		margin-bottom: 0;
+	}
+
+	h1 {
+		font-size: 90px;
+		margin-top: 30px;
+	}
+
+	h2 {
+		font-size: 40px;
+		margin-top: 0;
+	}
+</style>
+
+<h1>GEDDITDUN</h1>
+<h2>TO-DOs</h2>
